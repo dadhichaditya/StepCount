99ae6b633d223e01e9249127c4e4d18dab42fb3e
Aditya Dadhich <dadhichaditya98@gmail.com>
2018-06-20 16:21:26 +0530

    Threshold added

--- app/src/main/java/com/example/aditya/stepcount/MainActivity.java
+++ app/src/main/java/com/example/aditya/stepcount/MainActivity.java
@@ -39,11 +39,15 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     TextView step;
     LineChart mChart;
     Boolean plotData=true;
-
+    float prev_acc=150f;
+    float prev_acc1=150f;
+    boolean firstpeak=true;
+    boolean firstvalley=true;
     private SensorManager sensorManager;
     private final static String TAG = "StepDetector";
 
     Thread thread;
+    Vibrator vibrator;
 
     private float   mLastDirections[] = new float[3*2];
     private float   mLastExtremes[][] = { new float[3*2], new float[3*2] };
@@ -55,7 +59,7 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
     int flag=0,flag_sma=0;
     float peak_value,valley_value;
     double acc,lpfilter;
-    float[] gravityValues = null,smooth_acc=null;
+    float[] gravityValues = new float[3],smooth_acc=new float[3];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -76,10 +80,10 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
             }
         });
-      //  vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
+          vibrator = (Vibrator) getSystemService(Context.VIBRATOR_SERVICE);
         sensorManager=(SensorManager)this.getSystemService(SENSOR_SERVICE);
         sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER), SensorManager.SENSOR_DELAY_GAME);
-      //  sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_NORMAL);
+        //  sensorManager.registerListener(this, sensorManager.getDefaultSensor(Sensor.TYPE_ORIENTATION), SensorManager.SENSOR_DELAY_NORMAL);
 
 
 
@@ -114,8 +118,8 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         YAxis leftAxis =mChart.getAxisLeft();
         leftAxis.setTextColor(Color.BLACK);
         leftAxis.setDrawGridLines(true);
-        leftAxis.setAxisMaximum(25f);
-        leftAxis.setAxisMinimum(-5f);
+        leftAxis.setAxisMaximum(17f);
+        leftAxis.setAxisMinimum(3f);
         leftAxis.setDrawGridLines(true);
 
         YAxis rightAxis=mChart.getAxisRight();
@@ -167,10 +171,36 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
         });
         thread.start();
     }
+
+    private boolean  detectPeak(float prev_acc,float prev_acc1,double acc)
+    {
+        if (prev_acc1>prev_acc&&prev_acc1>acc){
+            if(firstpeak){
+                peak_value=prev_acc1;
+                firstpeak=false;
+            }
+
+            Log.i("Normal Peak Detected","YES");
+            if(prev_acc>10.5)
+            {
+                Log.i("INFO","Valid Peak Detected");
+                peak_value=prev_acc1;
+                return true;
+            }
+        }
+        return false;
+
+    }
+    private boolean  detectValley(float prev_acc,float prev_acc1,double acc)
+    {
+        return false;
+
+    }
     private void addEntry(SensorEvent event){
 
         LineData data=mChart.getData();
         Sensor sensor =event.sensor;
+
 //        if(gravityValues!=null)
 //        {
 //            gravityValues[0]=(float)(0.9*gravityValues[0]+0.1*event.values[0]);
@@ -180,23 +210,53 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 //            smooth_acc[0]=event.values[0]-gravityValues[0];
 //            smooth_acc[1]=event.values[1]-gravityValues[1];
 //            smooth_acc[2]=event.values[2]-gravityValues[2];
+//            Log.i("smoothened",smooth_acc[0]+" "+smooth_acc[1]+" "+smooth_acc[2]);
 //
 //        }
+//        if(smooth_acc!=null)
         acc=Math.sqrt(Math.pow(event.values[0],2)+Math.pow(event.values[1],2)+Math.pow(event.values[2],2));
-
-        if(data!=null)
+        if(prev_acc==150f)
         {
-            ILineDataSet set =data.getDataSetByIndex(0);
-            if(set==null){
-                set=createset();
-                data.addDataSet(set);
-            }
-            data.addEntry(new Entry(set.getEntryCount(),(float)(acc)),0);
-            data.notifyDataChanged();
-            mChart.notifyDataSetChanged();
-            mChart.setVisibleXRangeMaximum(250);
-            mChart.moveViewToX(data.getEntryCount());
+                prev_acc=(float)acc;
+                Log.i(" Noted First Acc",""+prev_acc);
         }
+        else
+            if(prev_acc1==150f){
+            prev_acc1=(float)acc;
+                Log.i(" Noted Second Acc",""+prev_acc1);
+
+            }
+        else
+        {
+            acc=acc*0.1+0.9*prev_acc1;
+
+
+            if(data!=null)
+            {
+                ILineDataSet set =data.getDataSetByIndex(0);
+                if(set==null){
+                    set=createset();
+                    data.addDataSet(set);
+                }
+                if(detectPeak(prev_acc,prev_acc1,acc))
+                {
+                        step_count++;
+
+                        step.setText(Integer.toString(step_count ));
+                        vibrator.vibrate(250);
+
+                }
+                prev_acc=prev_acc1;
+                prev_acc1=(float)acc;
+                data.addEntry(new Entry(set.getEntryCount(),(float)acc),0);
+                data.notifyDataChanged();
+                mChart.notifyDataSetChanged();
+                mChart.setVisibleXRangeMaximum(250);
+                mChart.moveViewToX(data.getEntryCount());
+            }
+        }
+
+
 
     }
 
@@ -208,30 +268,28 @@ public class MainActivity extends AppCompatActivity implements SensorEventListen
 
     public void onAccuracyChanged(Sensor arg0, int arg1) {
 
-        }
+    }
 
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-//            if(event.sensor.getType()==Sensor.TYPE_GRAVITY){
-//                gravityValues=event.values;
-//            }
-//            else {
-//                if (event.sensor.getType()==Sensor.TYPE_ACCELEROMETER&&plotData) {
-                if(plotData) {
-                    addEntry(event);
-                    plotData = false;
-                }
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        if (event.sensor.getType() == Sensor.TYPE_GRAVITY) {
+            gravityValues = event.values;
+        } else {
+            if (event.sensor.getType() == Sensor.TYPE_ACCELEROMETER && plotData) {
+
+                addEntry(event);
+                plotData = false;
+            }
 //                }
 //            }
         }
 
 
-
+    }
 
     public void onStop() {
         super.onStop();
         sensorManager.unregisterListener((SensorEventListener)this);
     }
-
 
 }
